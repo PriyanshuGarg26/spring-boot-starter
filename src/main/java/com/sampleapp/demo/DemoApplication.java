cba9af742c93eb93149f89bc0e7cf5a73fcc7d2f
Priyanshu Garg <87513713+PriyanshuGarg26@users.noreply.github.com>
2024-03-29 14:35:06 +0530

    Update DemoApplication.java

--- src/main/java/com/sampleapp/demo/DemoApplication.java
+++ src/main/java/com/sampleapp/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.sampleapp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(){
+		return "Hello";
+	}
 }
